c3c7bb454e8bc27ad9897ce81a2b63084d7709f2
Stefan <61718109+stefanmrvic@users.noreply.github.com>
2024-08-02 04:54:03 +0200

    Add: Slider that controls the grid size

--- main.js
+++ main.js
@@ -1,35 +1,70 @@
 window.addEventListener('load', () => {
     const grid = document.querySelector('.grid');
     
-    let gridSize = 6;
+    let gridSize = document.querySelector('#slider');
+    let sliderLabel = document.querySelectorAll('.button__slider--value');
 
-    for(let i = 0; i < gridSize; i++) {
-        for(let j = 0; j < gridSize; j++) {
-            const newDiv = document.createElement('div');
-            newDiv.classList.add('grid__element');
-            grid.appendChild(newDiv);
-        }
+    drawGrid();
+
+    gridSize.addEventListener('mousedown', () => {
+        gridSize.addEventListener('mousemove', setGridAndLabel);
+    })
+
+    gridSize.addEventListener('change', setGridAndLabel);
+
+    function setGridAndLabel() {
+        gridSize = document.querySelector('#slider');
+        setSliderLabel();
+        removeOldGrid();
+        drawGrid();
     }
 
-    const totalGridWidth = 560;
-    const gridElements = document.querySelectorAll('.grid__element');
+    function setSliderLabel() {
+        sliderLabel[0].innerHTML = gridSize.value;
+        sliderLabel[1].innerHTML = gridSize.value;
+    }
 
-    let gridElementWidth = totalGridWidth / gridSize;
-    
-    gridElements.forEach(element => {
-        element.style.flexBasis = `${gridElementWidth}px`;
+    document.body.addEventListener('mouseup', () => {
+        gridSize.removeEventListener('mousemove', setGridAndLabel);
     })
+
+    function removeOldGrid() {
+        const divs = document.querySelectorAll('.grid__element');
+
+        divs.forEach(div => {
+            div.remove();
+        })
+    }
+
+    function drawGrid() {
+        for(let i = 0; i < gridSize.value; i++) {
+            for(let j = 0; j < gridSize.value; j++) {
+                const newDiv = document.createElement('div');
+                newDiv.classList.add('grid__element');
+                grid.appendChild(newDiv);
+            }
+        }
+
+        const totalGridWidth = 560;
+        const gridElements = document.querySelectorAll('.grid__element');
+    
+        const gridElementWidth = totalGridWidth / gridSize.value;
+        
+        gridElements.forEach(element => {
+            element.style.flexBasis = `${gridElementWidth}px`;
+        })
+    }
 
     grid.addEventListener('mousedown', (e) => {
         grid.addEventListener('mousemove', changeBackground);
         e.preventDefault();
     });
 
-    grid.addEventListener('mouseup', () => {
+    document.body.addEventListener('mouseup', () => {
         grid.removeEventListener('mousemove', changeBackground);
     });
 
     function changeBackground(e) {
-        e.target.style.backgroundColor = "pink";
+        e.target.style.backgroundColor = "black";
     }
 })
